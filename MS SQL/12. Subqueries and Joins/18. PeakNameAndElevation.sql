03deaac354bedb89e5664e25e0b9ff06ca209f5e
TodorChankov <TodorChankov@students.softuni.bg>
2024-05-07 19:00:43 +0300

    Task 18 refactored to take only the highest peaks from country

--- MS SQL/12. Subqueries and Joins/18. PeakNameAndElevation.sql
+++ MS SQL/12. Subqueries and Joins/18. PeakNameAndElevation.sql
@@ -1,22 +1,31 @@
 SELECT TOP (5)
-    c.CountryName,
+    CountryName AS Country,
     CASE 
-		WHEN p.PeakName IS NULL THEN '(no highest peak)' 
-		ELSE p.PeakName
+		WHEN PeakName IS NULL THEN '(no highest peak)' 
+		ELSE PeakName
 	END AS [Highest Peak Name],
     CASE
-        WHEN p.Elevation IS NULL THEN 0 
-		ELSE p.Elevation
+        WHEN Elevation IS NULL THEN 0 
+		ELSE Elevation
     END AS [Highest Peak Elevation],
     CASE 
-        WHEN m.MountainRange IS NULL THEN '(no mountain)'
-        ELSE m.MountainRange
+        WHEN MountainRange IS NULL THEN '(no mountain)'
+        ELSE MountainRange
     END AS [Mountain]
-FROM Countries AS c
-LEFT JOIN MountainsCountries AS mc
-ON c.CountryCode = mc.CountryCode
-LEFT JOIN Mountains AS m
-ON mc.MountainID = m.ID
-LEFT JOIN Peaks AS p
-ON m.ID = p.MountainID
-ORDER BY c.CountryName, p.PeakName;
+FROM(
+    SELECT 
+        c.CountryName, 
+        p.PeakName, 
+        p.Elevation, 
+        m.MountainRange,
+        DENSE_RANK() OVER (PARTITION BY c.CountryName ORDER BY p.Elevation DESC) AS PeakRanks
+    FROM Countries AS c
+        LEFT JOIN MountainsCountries AS mc
+        ON c.CountryCode = mc.CountryCode
+        LEFT JOIN Mountains AS m
+        ON mc.MountainID = m.ID
+        LEFT JOIN Peaks AS p
+        ON m.ID = p.MountainID
+    ) AS subquery
+WHERE PeakRanks = 1
+ORDER BY Country, [Highest Peak Name]
